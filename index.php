3871f735553e7a9254446f219381739a954bc81a
Dahmani <dahmani.sof@gmail.com>
2016-08-25 15:53:53 +0200

    changement header violet

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
-<?php 
+<?php
 $index = true;
 include 'head.php';
 ?>
 	<div class="page">
 
 
-
+<a href="#">
 <div class="animation">
 	<h1>Animation</h1>
 </div>
+</a>
+<a href="#">
 <div class="transition">
 	<h1>Transition</h1>
 </div>
-
+</a>
 
 
 	</div> <!-- END PAGE -->
