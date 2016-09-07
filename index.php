4990eadd032d22435695c172e2eb1237ea28f03c
simplon-sofianeD <dahmani.sof@gmail.com>
2016-09-07 10:03:46 +0200

    test ssh

--- index.php
+++ index.php
@@ -20,7 +20,8 @@ include 'head.php';
 
 
 </div> <!-- END PAGE -->
-<div id="balles">
+ <div id="balles">
+ <!--
 <div class="ball1" id="ball100">
 </div>
 <div class="ball1" id="ball101">
@@ -37,7 +38,33 @@ include 'head.php';
 </div>
 <div class="ball2" id="ball201">
 </div>
+-->
 </div>
+<script>
+1
+var CreateBalls = {
+	pickColor: "",
+	obj:"",
+	color: ["red","green","yellow","blue","purple","grey","black","orange","pink"],
+	randomColor: function() {
+		var randomNumber =  Math.floor(Math.random()* this.color.length);
+	 	this.pickColor = this.color[randomNumber];
+	},
+	ball: function() {
+		this.randomColor();
+		this.obj = document.createElement('div');
+		this.obj.innerText = "LOL";
+		this.obj.id = "ball1";
+		this.obj.style.backgroundColor = this.pickColor;
+		var container = document.querySelector('#balles');
+		// container.appendChild(this.obj);
+	}
 
+}
+// setInterval(function(){ CreateBalls.ball();console.log(CreateBalls.obj); }, 1000);
+
+CreateBalls.ball();
+console.log(CreateBalls.obj);
+</script>
 
 <?php include 'footer.php'; ?>
